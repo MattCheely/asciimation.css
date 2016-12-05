f093ba982b087b4b2a054d57e91fc88a1a3ea951
Matthew Cheely <matt.cheely@gmail.com>
2016-12-04 21:22:12 -0500

    Fix deploy script

--- scripts/deploy-demo.sh
+++ scripts/deploy-demo.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
+BASEDIR=$(dirname "$0")
+PROJECT_DIR=$(dirname ${BASEDIR})
+SITE_DIR=${PROJECT_DIR}/github.io
 
-SITE_DIR=github.io
-
+pushd ${PROJECT_DIR}
 git clone git@github.com:MattCheely/MattCheely.github.io.git ${SITE_DIR}
 rm -r ${SITE_DIR}/asciimation.css
+mkdir ${SITE_DIR}/asciimation.css
+gulp
 cp -R dist/* ${SITE_DIR}/asciimation.css
-cd ${SITE_DIR}
+pushd ${SITE_DIR}
 git add .
 git commit -m "Update asciimation.css demo"
 git push
+popd
+rm -rf ${SITE_DIR}
+popd
